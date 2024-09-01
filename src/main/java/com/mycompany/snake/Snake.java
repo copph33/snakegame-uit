31c4a49169b2d77d27faac01d6aa698349e33075
vynguyen20121997 <133662722+vynguyen20121997@users.noreply.github.com>
2024-09-01 18:52:26 +0700

    First snake game version!

---
+++ src/main/java/com/mycompany/snake/Snake.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.snake;
+
+/**
+ *
+ * @author Admin
+ */
+public class Snake {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new GameFrame();
+    }
+}
